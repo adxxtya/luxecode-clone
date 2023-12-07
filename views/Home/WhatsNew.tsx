8fc0c958d93ac7c45b2d9906844d4581acdec5d2
adxxtya <andronaut9@gmail.com>
2023-12-07 15:13:34 +0530

    Fixing Errors

--- views/Home/WhatsNew.tsx
+++ views/Home/WhatsNew.tsx
@@ -1,20 +1,28 @@
+import { client } from "@/lib/sanity";
 import Image from "next/image";
 import React from "react";
 
-const WhatsNew = () => {
+async function getData() {
+  const query = `*[_type == "post" && overview == "Places" && overview == "Art" && overview == "People" && overview == "Things"]`;
+  const data = await client.fetch(query);
+  return data;
+}
+
+const WhatsNew = async () => {
+  const data = await getData();
   return (
     <div className="flex w-full justify-center">
       <div className="container w-[60%] mt-12 ">
         <div className="text-6xl font-semibold font-serif">What&apos;s new</div>
         <div className="grid grid-cols-2 gap-8 mt-10 place-items-center">
-          {[1, 2, 3, 4, 5, 6].map((item) => (
-            <div className="col-span-1 w-full h-[65vh] relative" key={item}>
+          {data.map((post: any) => (
+            <div className="col-span-1 w-full h-[65vh] relative" key={post}>
               <div className="w-full z-10 text-white flex gap-2 flex-col justify-end items-start h-full pl-6 pb-8">
                 <div className="z-30 font-light text-sm text-gray-200 hover:text-white cursor-pointer">
-                  THINGS
+                  {post.overview}
                 </div>
                 <div className="z-30 font-semibold text-3xl cursor-pointer hover:text-gray-200">
-                  Beauty In Nature
+                  {post.title}
                 </div>
               </div>
               <Image
@@ -26,7 +34,7 @@ const WhatsNew = () => {
               />
               <div className="absolute top-0 left-0 w-full h-full bg-black opacity-60 -z-20" />
               <div className="absolute top-8 left-8 w-full h-full z-20 text-sm text-white font-serif">
-                10 Oct, 2023
+                {new Date(post._createdAt).toISOString().split("T")[0]}
               </div>
             </div>
           ))}
